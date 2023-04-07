b0e497c3bf15c5456940414c28dc2d6150b92b1f
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-04-07 19:13:01 +0100

    I added some console.logs to have a better understanding of how this pseudocode works.

--- engine27FromCodewars.js
+++ engine27FromCodewars.js
@@ -2,12 +2,14 @@ function numberOfPairs(gloves) {
     var pairs = 0,
         counts = {};
     for (var color of gloves) {
+        console.log("color:", color)
         if (!counts.hasOwnProperty(color)) {
-            console.log(counts)
+            console.log("first, before:", counts)
             counts[color] = 0;
+            console.log("first, after:", counts)
         }
         if (++counts[color] === 2) {
-            console.log(counts)
+            console.log("second:", counts)
             counts[color] -= 2;
             pairs++;
         }
